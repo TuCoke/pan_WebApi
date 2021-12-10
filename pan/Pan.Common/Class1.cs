8a32ee653e5b17b8561b0d5558733bbcaab4e669
hongbowork <hongbowork@outlook.com>
2021-12-10 18:01:28 +0800

    初始化项目

---
+++ pan/Pan.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pan.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ pan/Pan.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pan.Domain
+{
+    public class Class1
+    {
+    }
+}
